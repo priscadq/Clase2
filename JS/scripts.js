a6d9f65cfe7c376055cc864cd4d821a96f284b23
PriscaDQ <priscadq@gmail.com>
2017-09-02 13:11:36 -0300

    ultumo update por ahora

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,39 +3,59 @@
     var productos = ["Tj Visa", "Tj MasterCard", "Caja de Ahorro", "Caja Corriente"];
 
    
-    do {
-        var nombre=prompt("Ingresar nombre");
-    } while (nombre == "");
-    
-    do {
-        var apellido=prompt("Ingresar apellido");
-    } while (nombre == "");
-
-    do {
-        var tpdocumento=prompt("Tipo Documento");
-    } while (nombre == "");
-
-    do {
-    var nrdocumento=prompt("Nro Documento");
-    } while (isNaN(nrdocumento));
-    
-    do {
-    var telefono=prompt("Ingresar Telefono");
-    } while (isNaN(telefono));
-
-    do {
-        var email=prompt("Ingresar email");
-    } while (nombre == "");
-
 
     function mostrarclienteyproductos() {
+
+
+        do {
+            var nombre=prompt("Ingresar nombre");
+        } while (nombre == "");
+        
+        do {
+            var apellido=prompt("Ingresar apellido");
+        } while (nombre == "");
+    
+        do {
+            var tpdocumento=prompt("Tipo Documento");
+        } while (nombre == "");
+    
+        do {
+        var nrdocumento=prompt("Nro Documento");
+        } while (isNaN(nrdocumento));
+        
+        do {
+        var telefono=prompt("Ingresar Telefono");
+        } while (isNaN(telefono));
+    
+        do {
+            var email=prompt("Ingresar email");
+        } while (nombre == "");
+
         document.getElementById("lblNombre").innerHTML = nombre; 
         document.getElementById("lblApellido").innerHTML = apellido; 
         document.getElementById("lblTipDoc").innerHTML = tpdocumento; 
         document.getElementById("lblNroDocumento").innerHTML = nrdocumento; 
         document.getElementById("lblTelefono").innerHTML = telefono; 
         document.getElementById("lblEmail").innerHTML = email; 
-        document.getElementById("tblProductos").innerHTML = productos; 
+        
         
         }
 
+
+
+function showProductos(){
+    var prods = getProductos();
+    document.getElementById("tblProductos").innerHTML = prods;
+
+}
+
+function getProductos(){
+    var columnas = "";
+    for (var i = 0; i < productos.length; i++ ) {
+        columnas = columnas + "<td>" + productos[i] + "</td>";
+    }
+    filas= "<tr>" + columnas + "</tr>";
+    return filas;
+  
+
+}
