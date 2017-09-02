dfe2bef58f99f51cacf994a062a56d0ad47022f9
PriscaDQ <priscadq@gmail.com>
2017-09-02 12:10:47 -0300

    segundo trabajo practico

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,17 +1,41 @@
 
-function verwhile {
-            var text = "";
-            var i = 0;
+    var nombre, apellido, nrdocumento, tpdocumento, telefono, email;
+    var productos = ["Tj Visa", "Tj MasterCard", "Caja de Ahorro", "Caja Corriente"];
 
-            do {
-                text += "<br> Number is " + i;
-                i++;
-                
-            }
-            while (i < 10) {
-                text += "<br> Number is " + i;
-                i++;
-            }
+   
+    do {
+        var nombre=prompt("Ingresar nombre");
+    } while (nombre == "");
+    
+    do {
+        var apellido=prompt("Ingresar apellido");
+    } while (nombre == "");
 
-            document.getElementById("demo").innerHTML = text;
+    do {
+        var tpdocumento=prompt("Tipo Documento");
+    } while (nombre == "");
+
+    do {
+    var nrdocumento=prompt("Nro Documento");
+    } while (isNaN(nrdocumento));
+    
+    do {
+    var telefono=prompt("Ingresar Telefono");
+    } while (isNaN(telefono));
+
+    do {
+        var email=prompt("Ingresar email");
+    } while (nombre == "");
+
+
+    function mostrarclienteyproductos() {
+        document.getElementById("lblNombre").innerHTML = nombre; 
+        document.getElementById("lblApellido").innerHTML = apellido; 
+        document.getElementById("lblTipDoc").innerHTML = tpdocumento; 
+        document.getElementById("lblNroDocumento").innerHTML = nrdocumento; 
+        document.getElementById("lblTelefono").innerHTML = telefono; 
+        document.getElementById("lblEmail").innerHTML = email; 
+        document.getElementById("tblProductos").innerHTML = productos; 
+        
         }
+
